87d867fd449c18e04fcde96d7cbf17d018ecc945
ohjiyeon3275 <ojy940716@gmail.com>
2022-09-25 21:05:03 +0900

    feat : rendering comments by status

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((data) => {
-    return <li key={data.id}>{data.content}</li>;
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "this comment is awaiting moderation...";
+    }
+
+    if (comment.status === "rejected") {
+      content = "this comment has been rejected!";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul> {renderedComments} </ul>;
